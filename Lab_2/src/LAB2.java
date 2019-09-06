b545553c7d24c3816136f52fe12866b669209f6c
Morgan <morganw2001@gmail.com>
2019-09-06 09:19:15 -0400

    finished lab 2
    find the sum of 2 numbers

---
+++ Lab_2/src/LAB2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class LAB2 {
+
+
+
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("enter a number");
+        int a = scanner.nextInt();
+
+        System.out.println("enter another number");
+        int b = scanner.nextInt();
+
+        int sum = a+b;
+
+        System.out.println(sum);
+
+
+    }
+
+
+
+}
