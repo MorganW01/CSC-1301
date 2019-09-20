f935cb6d6c51c1555090b427d4ba6fe1997563b3
Morgan <morganw2001@gmail.com>
2019-09-20 10:27:11 -0400

    finished BoughtMeACat

---
+++ Lab_4/src/BoughtMeACat.java
@@ -0,0 +1,57 @@
+public class BoughtMeACat {
+
+    public static void main(String []args){
+        catVerse();
+        henVerse();
+        duckVerse();
+        gooseVerse();
+        sheepVerse();
+    }
+
+
+    public static void catVerse() {
+        System.out.println("Bought me a cat and the cat pleased me,\n" +
+                "I fed my cat under yonder tree.\n" +
+                "Cat goes fiddle-i-fee.\n");
+    }
+
+    public static void henVerse(){
+        System.out.println("Bought me a hen and the hen pleased me,\n" +
+                "I fed my hen under yonder tree.\n" +
+                "Hen goes chimmy-chuck, chimmy-chuck,\n"+
+                "Cat goes fiddle-i-fee.\n");
+
+
+    }
+
+    public static void duckVerse(){
+        System.out.println("Bought me a duck and the duck pleased me,\n" +
+                "I fed my duck under yonder tree.\n" +
+                "Duck goes quack, quack,\n"+
+                "Hen goes chimmy-chuck, chimmy-chuck,\n"+
+                "Cat goes fiddle-i-fee.\n");
+
+
+    }
+
+    public static void gooseVerse(){
+        System.out.println("Bought me a goose and the goose pleased me,\n" +
+                "I fed my goose under yonder tree.\n" +"Goose goes hissy, hissy,\n"+
+                        "Duck goes quack quack,\n"+
+                "Hen goes chimmy-chuck, chimmy-chuck,\n"+
+                "Cat goes fiddle-i-fee.\n");
+
+    }
+
+    public static void sheepVerse(){
+        System.out.println("Bought me a sheep and the sheep pleased me,\n" +
+                "I fed my sheep under yonder tree.\n"+"Sheep goes baa, baa,\n"+
+                "Goose goes hissy, hissy,\n"+
+                "Duck goes quack quack,\n"+
+                "Hen goes chimmy-chuck, chimmy-chuck,\n"+
+                "Cat goes fiddle-i-fee.\n");
+
+    }
+
+
+}
