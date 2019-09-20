f0dc9c864f04f6b46a1b1b7f04f955397eca8236
Morgan <morganw2001@gmail.com>
2019-09-20 10:02:46 -0400

    finished Stewie.java

---
+++ Lab_4/src/Stewie.java
@@ -0,0 +1,30 @@
+public class Stewie {
+
+    public static void main (String [] args){
+        forwardSlashLine();
+        stewieCatchphrase();
+        backSlashLine();
+        stewieCatchphrase();
+        backSlashLine();
+        stewieCatchphrase();
+        backSlashLine();
+        stewieCatchphrase();
+        backSlashLine();
+
+    }
+
+    public static void backSlashLine(){
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+
+    }
+
+    public static void forwardSlashLine(){
+        System.out.println("//////////////////////");
+
+    }
+
+    public static void stewieCatchphrase(){
+
+        System.out.println("|| Victory is mine!||");
+    }
+}
