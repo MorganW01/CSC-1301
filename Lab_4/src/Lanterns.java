b0acca88d8a9317a7fc0e7fadd5b79963c4209fd
Morgan <morganw2001@gmail.com>
2019-09-20 10:13:10 -0400

    finished Lanterns.java

---
+++ Lab_4/src/Lanterns.java
@@ -0,0 +1,28 @@
+public class Lanterns {
+
+    public static void main(String [] args){
+
+        pyramidOfStars();
+        pyramidOfStars();
+        verticalBarRow();
+        pyramidOfStars();
+        pyramidOfStars();
+
+
+    }
+    // draws pyramid-like shape made with stars
+    public static void pyramidOfStars(){
+        System.out.println("\t*****");
+        System.out.println("  *********"); //work on centering this one
+        System.out.println("*************");
+    }
+
+    public static void verticalBarRow(){
+        System.out.println("* | | | | | *");
+        System.out.println("*************");
+
+
+    }
+
+
+}
