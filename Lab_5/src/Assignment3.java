1a4ed7b40617faf6fbe9e63a2d40830ce4476f9c
Morgan <morganw2001@gmail.com>
2019-09-27 10:42:20 -0400

    finish placing rockets side by side

---
+++ Lab_5/src/Assignment3.java
@@ -0,0 +1,53 @@
+public class Assignment3 {
+
+    public static void main (String [] args){
+
+        drawUpArrow();
+        drawALine();
+        drawBars();
+        drawALine();
+        drawTextBars();
+        drawALine();
+        drawBars();
+        drawALine();
+        drawUpArrow();
+
+
+    }
+
+    public static void drawUpArrow() {
+
+        //done
+        String firstRow = "   /\\";
+
+        String secondRow = "  /\t \\";
+
+        //done
+        String thirdRow = " /\t  \\";
+
+        System.out.println(firstRow);
+        System.out.println(secondRow);
+        System.out.println(thirdRow);
+
+    }
+
+    public static void drawALine(){
+        String line = "+------+  +------+";
+        System.out.println(line);
+
+    }
+
+    public static void drawBars() {
+        String bars = "|      |\n|      |  |      |";
+        System.out.println(bars);
+
+        //may need to add just six spaces
+
+    }
+
+    public static void drawTextBars() {
+        String textBars = "|United|\n|States|";
+        System.out.println(textBars);
+
+    }
+}
