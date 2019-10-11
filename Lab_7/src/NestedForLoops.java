a2ca4054d4389b0ddabb9f660ab869d55215d1bf
Morgan <morganw2001@gmail.com>
2019-10-11 18:22:16 -0400

    finished question9

--- Lab_7/src/NestedForLoops.java
+++ Lab_7/src/NestedForLoops.java
@@ -1,26 +1,27 @@
 public class NestedForLoops {
 
     //question 9
-    public static final int SEGMENT=3;
-    public static final int HEIGHT=4;
+    public static final int SEGMENT = 2;
+    public static final int HEIGHT = 6;
 
-    public static void main(String []args){
-        //initially line =1
-        for (int line = 1; line <= HEIGHT; line++) {
-            for (int j = 1; j <= (-1* line + HEIGHT); j++) {
-                System.out.print(" ");
-            }
-            //System.out.print("*");
+    public static void main(String []args) {
 
-            for (int i = 2; i <= 2 * line - 1; i++) {
-                System.out.print("*");
+        for (int i = 1; i <= SEGMENT; i++) {
+            for (int line = 1; line <= HEIGHT; line++) {
+                for (int j = 1; j <= (-1 * line + HEIGHT); j++) {
+                    System.out.print(" ");
+                }
+                //System.out.print("*");
+
+                for (int k = 2; k <= 2 * line - 1; k++) {
+                    System.out.print("*");
+                }
+                System.out.println("*");
             }
-            System.out.println("*");
+
+
         }
-
-
     }
-
 
 
 
