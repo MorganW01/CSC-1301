29dfc480006570bedebc5401f53d874e7f28d047
Morgan <morganw2001@gmail.com>
2019-10-18 09:38:46 -0400

    finished questions 1 and 3

--- Lab_8/src/Lab8.java
+++ Lab_8/src/Lab8.java
@@ -2,33 +2,53 @@ public class Lab8 {
 
     public static void main (String []args){
 
-
-
+        //question1();
+        question2();
+        //question3();
 
     }
 
-    public static void question1(){
+    public static void question1() {
+
+            for (int i = 1; i <= 4; i++) {
+
+                for (int j = 1; j <= 5; j++) {
+
+                    System.out.print(Math.round(Math.pow(i, j))+" ");
+                }
+                System.out.println(" ");
 
 
+            }
     }
 
     public static void question2(){
+        for(int i=1; i<=4;i++){}
+
+
 
 
     }
 
 
-    public static final int SEGMENT =3;
-    public static final int HEIGHT =4;
+    public static final int SEGMENT =2;
+    public static final int HEIGHT =6;
 
-    public static void question3(){
-        for (int i=1; i<=SEGMENT; i++){
-            for (int j=7){}
+    public static void question3() {
+
+        for (int i = 1; i <= SEGMENT; i++) {
+            for (int line = HEIGHT; line >= 1; line--) {
+                for (int j = 1; j <= (-1 * line + HEIGHT); j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 2; k <= 2 * line - 1; k++) {
+                    System.out.print("*");
+                }
+                System.out.println("*");
+            }
+
 
         }
-
-
-
 
     }
 }
