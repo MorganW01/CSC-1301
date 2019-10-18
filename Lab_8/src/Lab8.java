eb52a182e0f694cbb3984f4afa331963b560ab5b
Morgan <morganw2001@gmail.com>
2019-10-18 09:18:19 -0400

    created lab8

---
+++ Lab_8/src/Lab8.java
@@ -0,0 +1,34 @@
+public class Lab8 {
+
+    public static void main (String []args){
+
+
+
+
+    }
+
+    public static void question1(){
+
+
+    }
+
+    public static void question2(){
+
+
+    }
+
+
+    public static final int SEGMENT =3;
+    public static final int HEIGHT =4;
+
+    public static void question3(){
+        for (int i=1; i<=SEGMENT; i++){
+            for (int j=7){}
+
+        }
+
+
+
+
+    }
+}
