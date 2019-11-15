12e45f4cfb3153c1338a43e0823a056cb29dac96
Morgan <morganw2001@gmail.com>
2019-11-15 10:32:40 -0500

    finish question 2

--- Lab_12/src/Lab12_2.java
+++ Lab_12/src/Lab12_2.java
@@ -1,12 +1,32 @@
+import java.util.Random;
 public class Lab12_2{
 
     public static void main (String []args){
+
+        Random rand = new Random();
+        //(max - min +1 )+ min (creates range)
+        int a = rand.nextInt(128-16+1)+16;
+
 
 
 
     }
 
-    //public static
+    public static int showTwos(int a){
+        if (a%2==0){
+            for (int i =0;i<=a;i++){
+
+                
+            }
+
+
+        }
+
+
+
+
+
+    }
 
 
 
