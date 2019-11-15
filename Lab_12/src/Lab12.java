5026e59ec1d1d610a7e28b3ef3ab29cb7df918be
Morgan <morganw2001@gmail.com>
2019-11-15 10:12:30 -0500

    done with question 1

---
+++ Lab_12/src/Lab12.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Lab12 {
+
+    public static void main(String[]args){
+        Random rand = new Random();
+        //(max - min +1 )+ min (creates range)
+        int a = rand.nextInt(9999-800+1)+800;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter an integer between 0 and 9");
+        int b = scanner.nextInt();
+
+        if (b>9||b<0){
+            System.out.println("Your number is invalid! Enter another one: ");
+            b=scanner.nextInt();
+        }
+
+        boolean flag = containDigits(a,b);
+        System.out.println("My name is Morgan, and " +a+ " contains "+b+" : "+flag);
+
+    }
+
+    public static boolean containDigits (int a, int b){
+        boolean flag= false;
+        while (a > 0){
+            if (a % 10 == b){
+
+                flag = true;
+                a=a/10;
+                break;
+            }
+            else {
+                flag=false;
+                break;
+            }
+        }
+        return flag;
+
+    }
+}
---
+++ Lab_12/src/Lab12_2.java
@@ -0,0 +1,15 @@
+public class Lab12_2{
+
+    public static void main (String []args){
+
+
+
+    }
+
+    //public static
+
+
+
+
+
+}
